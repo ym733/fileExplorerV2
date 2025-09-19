b0096e22218563d3bd5c79d2c60306b5fd4e7d82
ym733 <ym7330694@gmail.com>
2025-09-19 22:49:55 +0300

    directory tree implementation

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,32 @@
 class MainController < ApplicationController
   def index
+    root_directory_path = Dir.pwd
+
+    def build_directory_tree(path)
+      items = []
+      Dir.foreach(path) do |item|
+        next if item[0] == '.' && File.directory?(item) # rubocop:disable Style/StringLiterals
+
+        item_path = File.join(path, item)
+
+        if File.directory?(item_path)
+          items << {
+            name: item,
+            path: item_path,
+            type: "directory",
+            children: build_directory_tree(item_path)
+          }
+        else
+          items << {
+            name: item,
+            path: item_path,
+            type: "file"
+          }
+        end
+      end
+      items
+    end
+
+    @tree = build_directory_tree root_directory_path
   end
 end
